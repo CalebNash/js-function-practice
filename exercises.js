181228a4c3468293f07678cb2d5ad50466b7c48a
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-09-02 16:21:31 -0400

    isVowel finished

--- exercises.js
+++ exercises.js
@@ -31,7 +31,7 @@ function maxOfThree(num1, num2, num3)
   {
     return num2;
   }
-  else()
+  else
   {
     return num3;
   }
@@ -41,11 +41,22 @@ function maxOfThree(num1, num2, num3)
   // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
   // ---------------------
 
-
+function isVowel(char)
+{
+  if(char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' || char === 'y')
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
 
   // ---------------------
   // Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
   // ---------------------
+  // Function return translated string
 
 
 
